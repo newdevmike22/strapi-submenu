ac1ab267cff78163bb8476944b2b85efd72c2758
MLDODSON <paola@dominicansoul.com>
2024-02-03 12:01:09 -0600

    Branch 7
    Branch 7 - Made updates to Submenu.jsx file.

--- submenu/src/components/Submenu.jsx
+++ submenu/src/components/Submenu.jsx
@@ -1,6 +1,29 @@
+import sublinks from "../data";
+import { useGlobalContext } from "../Context";
+
 const Submenu = () => {
+    const {pageId} = useGlobalContext();
+    const currentPage = sublinks.find((item => item.pageId === pageId));
+
     return (
-        <h1>Submenu</h1>
+        <div className={currentPage ? 'submenu show-submenu' : 'submenu'}>
+            <h5 className="text-[1.25rem]">{currentPage?.page}</h5>
+            <div 
+                className="submenu-links" 
+                style={{gridTemplateColumns:currentPage?.links?.length > 3 ? '1fr' : '1fr'}}
+            >
+                {currentPage?.links?.map((link) => {
+                    const {id, url, label, icon} = link;
+
+                    return (
+                        <a key={id} href={url}>
+                            {icon}
+                            {label}
+                        </a>
+                    )
+                })}
+            </div>
+        </div>
     )
 };
 
