51a4cd2d2bcff839205996d7ed85c5be03aa9117
MLDODSON <paola@dominicansoul.com>
2024-02-03 13:18:41 -0600

    Branch 8
    Branch 8 - Hiding the submenu part 2 and final completed project.

--- submenu/src/components/Navbar.jsx
+++ submenu/src/components/Navbar.jsx
@@ -15,10 +15,10 @@ const Navbar = () => {
         <nav 
             onMouseOver={handleSubmenu}
             className="h-[5rem] flex items-center justify-center">
-            <div className="w-[90vw] max-w-[1120px] flex justify-between items-center">
+            <div className="nav-center">
                 <h3 className="normal-case text-white text-[2rem] tracking-[3px] font-bold">strapi</h3>
                 <button 
-                    className="w-8 h-8 bg-white text-[#4338ca] rounded-md border-transparent transition-all duration-300 ease-in-out cursor-pointer hover:scale-110 toggle-btn"
+                    className="toggle-btn"
                     onClick={openSidebar}
                 >
                     <FaBars className="mx-auto" />
